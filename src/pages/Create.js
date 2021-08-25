8fa6d57993958e292f721896ea48cf3fa249056b
5amuel <samuel.martinez.morales@gmail.com>
2021-08-25 17:38:41 -0500

    text fields added and state for their usage

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -7,51 +7,92 @@ import { AcUnitOutlined } from '@material-ui/icons'
 // import { Send } from '@material-ui/icons'
 import { KeyboardArrowRight } from '@material-ui/icons/';
 import { makeStyles } from '@material-ui/styles'
+import { TextField } from '@material-ui/core';
+import { useState } from 'react'
+
 
 
 
 const useStyles = makeStyles({
-  btn: {
-    fontSize: 60,
-    backgroundColor: "violet",
-    "&:hover" : {
-      backgroundColor: "blue"
-    }
-  },
-
-  title: {
-    textDecoration: "underline",
-    marginBottom: 20
+  field:{
+    marginTop:20,
+    marginBottom:20,
+    display: 'block'
   }
 })
 
 
 export default function Create() {
-const classes = useStyles()
- 
+  const classes = useStyles()
+  const {field} = classes
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (title == ''){
+      setTitleError(true)
+    }
+    if (details == ''){
+      setDetailsError(true)
+    }
+    if(title && details){
+      console.log(title, details)
+      setDetailsError(false)
+      setTitleError(false)
+    }
+
+  }
 
   return (
     <Container>
       <Typography 
-        className={classes.title}
+        // className={classes.title}
         variant="h6"
         component="h2"
         color="textSecondary"
         gutterBottom
       >
-        Creae a New Note
+        Create a New Note
       </Typography>
-      <Button 
-        // className={classes.btn}
-        type="submit" 
-        color="secondary" 
-        variant="contained"
-        // startIcon={<Send />}
-        endIcon={<KeyboardArrowRight />}
-        onClick={() => console.log('clicked')}
-      >Submit</Button>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField 
+          onChange={(e) => setTitle(e.target.value)}
+          className={field}
+          label="Note Title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          required
+          error={titleError}
+         />
+         <TextField 
+         onChange={(e) => setDetails(e.target.value)}
+          className={field}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+         />
+         <Button 
+            // className={classes.btn}
+            type="submit" 
+            color="secondary" 
+            variant="contained"
+            // startIcon={<Send />}
+            endIcon={<KeyboardArrowRight />}
+          >Submit</Button>
+      </form>
+      
       <br />
-      <AcUnitOutlined color="secondary" fontSize="small"/>
+      {/* <AcUnitOutlined color="secondary" fontSize="small"/> */}
     </Container>
   )
 }
