df2704ec1a8dc9994655d7acbc2a3037d0ad0214
5amuel <samuel.martinez.morales@gmail.com>
2021-08-25 12:37:46 -0500

    implementation of makeStyles hook

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,15 +1,38 @@
 import React from 'react'
 import { Typography } from '@material-ui/core'
 import { Button } from '@material-ui/core'
-// import { ButtonGroup } from '@material-ui/core'
+import { ButtonGroup } from '@material-ui/core'
 import { Container } from '@material-ui/core'
 import { AcUnitOutlined } from '@material-ui/icons'
-import { Send } from '@material-ui/icons'
+// import { Send } from '@material-ui/icons'
 import { KeyboardArrowRight } from '@material-ui/icons/';
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: "violet",
+    "&:hover" : {
+      backgroundColor: "blue"
+    }
+  },
+  title: {
+    textDecoration: "underline",
+    marginBottom: 20
+  }
+    
+  
+})
+
+
 export default function Create() {
+const classes = useStyles()
+ 
+
   return (
     <Container>
       <Typography 
+        className={classes.title}
         variant="h6"
         component="h2"
         color="textSecondary"
@@ -18,6 +41,7 @@ export default function Create() {
         Creae a New Note
       </Typography>
       <Button 
+        className={classes.btn}
         type="submit" 
         color="primary" 
         variant="contained"
